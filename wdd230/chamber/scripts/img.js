adbf10a59be1a67ced37e485d1bb22e5ff763e0b
antonioyed <yed21001@byui.edu>
2022-10-28 13:30:49 -0500

    done

--- wdd230/chamber/scripts/img.js
+++ wdd230/chamber/scripts/img.js
@@ -32,3 +32,42 @@ else {
         lazyLoad(img)
     })
 }
+
+
+
+// -------------- Visit Difference calculator -------------- 
+// initialize display elements
+const difVisit = document.querySelector(".difVisit");
+const pElement = document.querySelector(".visits p");
+
+const minute = 1000 * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+let lastVisit = Number(window.localStorage.getItem("lastVisit"));
+const d = new Date();
+const time = d.getTime();
+const dif = (time - lastVisit);
+
+const difSeconds = Math.round(dif/1000);
+const difMinutes = Math.round(dif/minute);
+const difHours = Math.round(dif/hour);
+const difDays = Math.round(dif/day);
+let timeText;
+
+
+timeText = difDays +" days ";  
+difVisit.textContent = timeText;
+
+
+// determine if this is the first visit or display the number of visits.
+if (dif !== 0) {
+	difVisit.textContent = timeText;
+} 
+else {
+	pElement.textContent = `This is your first visit!`;
+}
+
+// store the this visit as the new last visit
+
+localStorage.setItem("lastVisit", time);
