0356e350d68cb46d82a8dd78c3839fb5e0187176
antonioyed <yed21001@byui.edu>
2022-11-11 06:48:46 -0500

    new

--- wdd230/chamber/scripts/join.js
+++ wdd230/chamber/scripts/join.js
@@ -1,18 +1,26 @@
- 
 const todayDate = new Date();
 
-// Get year, month, and day part from the date
 var year = todayDate.toLocaleString("default", { year: "numeric" });
 var month = todayDate.toLocaleString("default", { month: "2-digit" });
 var day = todayDate.toLocaleString("default", { day: "2-digit" });
 
 
-
-
-// Generate yyyy-mm-dd date string
 var formattedDate = year + "-" + month + "-" +day ;
-const formDate = document.getElementById("datejoi");
-formDate.setAttribute("value", formattedDate); //Breaks on thankyou.html
+const formDate = document.getElementById("date");
+formDate.setAttribute("value", formattedDate); 
+
+
+function holdName(){
+    const formName = document.querySelector('[name="fname"]').value;
+    localStorage.setItem("submitName", formName);
+}
+
+const subBtn = document.querySelector(".submitBtn");
+
+subBtn.addEventListener("click", holdName);
+console.log("test");
+let currentName = window.localStorage.getItem("submitName");
+document.getElementById("submitee-name").textContent = currentName;
 
 
 
