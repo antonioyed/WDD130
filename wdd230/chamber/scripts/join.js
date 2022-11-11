02cdd7e15aba8e0ff261cd19462149079e8f9028
antonioyed <yed21001@byui.edu>
2022-11-10 19:47:26 -0500

    new

--- wdd230/chamber/scripts/join.js
+++ wdd230/chamber/scripts/join.js
@@ -1,25 +1,6 @@
-const todayDate = new Date();
 
-// Get year, month, and day part from the date
-var year = todayDate.toLocaleString("default", { year: "numeric" });
-var month = todayDate.toLocaleString("default", { month: "2-digit" });
-var day = todayDate.toLocaleString("default", { day: "2-digit" });
+const cD = new Date();
+const fDate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(cD);
 
-// Generate yyyy-mm-dd date string
-var formattedDate = year + "-" + month + "-" +day ;
-const formDate = document.getElementById("datejoi");
-formDate.setAttribute("value", formattedDate); //Breaks on thankyou.html
-
-// Hold Submittee 
-function holdName(){
-    const formName = document.querySelector('[name="fname"]').value;
-    localStorage.setItem("submitName", formName);
-}
-
-const subBtn = document.querySelector(".submitBtn");
-
-subBtn.addEventListener("click", holdName); //Breaks on thankyou.html
-console.log("test");
-let currentName = window.localStorage.getItem("submitName");
-document.getElementById("submitee-name").textContent = currentName;
-
+const currentHr = cD.getHours() + ':' + cD.getMinutes();
+document.getElementById('a_date').value = `${fDate} ${currentHr}`
