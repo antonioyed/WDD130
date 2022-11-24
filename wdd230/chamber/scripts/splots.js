45c3aafe5ac940899ebb7c03154ee399b8e9efc2
antonioyed <yed21001@byui.edu>
2022-11-24 00:30:03 -0500

    new

--- wdd230/chamber/scripts/splots.js
+++ wdd230/chamber/scripts/splots.js
@@ -21,29 +21,29 @@ const displaysplots = (splot)=>{
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
     let portrait = document.createElement('img');
-    let address = document.createElement('p');
-    let website = document.createElement('a');
+    let email = document.createElement('p');
+    let description =document.createElement('p');
     let phone = document.createElement('p');
-    let membershipLevel = document.createElement('p');
+    
 
     h2.textContent = `${splot.name}`;
-    address.textContent = `Address: ${splot.address}`;
+    address.textContent = `Address: ${splot.email}`;
     website.textContent = `Website: ${splot.website}`;
     phone.textContent = `Phone Number: ${splot.phone}`;
-    membershipLevel.textContent = `Membership Level: ${splot.membershipLevel}`;
+    description.textContent = `description:  ${splot.description}`;
 
     portrait.setAttribute('src', splot.imageurl);
     portrait.setAttribute('alt',`Portrait of ${splot.name} - ${splot.order}th Latter-day President`);
     portrait.setAttribute('loading', 'lazy');
-    website.setAttribute('href',`${splot.website}`);
+    
 
 
     card.appendChild(h2);
     card.appendChild(portrait);
-    card.appendChild(address);
-    card.appendChild(website)
+    card.appendChild(email);
+    card.appendChild(description);
     card.appendChild(phone);
-    card.appendChild(membershipLevel);
+    
     
     
 
