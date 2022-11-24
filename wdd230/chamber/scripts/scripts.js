bb897e403ee34a7ff23b8e52521f0f6c873a7c8b
antonioyed <yed21001@byui.edu>
2022-11-24 18:48:29 -0500

    new

--- wdd230/chamber/scripts/scripts.js
+++ wdd230/chamber/scripts/scripts.js
@@ -1,3 +1,117 @@
+
+
+
+const URL = "https://antonioyed.github.io//WDD130/wdd230/chamber/json/data.json";
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.getElementById("cards");
+
+function buildBusinessCards(info, type) {
+  let data = info.businesses;
+  data.forEach((business) => {
+    let card = document.createElement("section");
+    let name = document.createElement("h2");
+    let p = document.createElement("p");
+    let p2 = document.createElement("p");
+    let a = document.createElement("a");
+    let level = document.createElement("h4");
+
+    card.setAttribute("id", "section");
+    name.innerHTML = `${business.name}`;
+    p.innerHTML = `Address: ${business.address}`;
+    p2.innerHTML = `Phone Number: ${business.phone}`;
+    a.innerHTML = ` Website:${business.website}`;
+    level.innerHTML = `Membership Level: ${business.membershipLevel}`;
+    a.setAttribute("href", `${business.website}`);
+
+    if (type == "grid") {
+      
+      let img = document.createElement("img");
+      img.setAttribute("src", `${business.imageurl}`);
+      img.setAttribute("alt", `${business.name}`);
+      img.setAttribute("loading", "lazy");
+      card.append(img);
+    } else {
+      let h2 = document.createElement("h2");
+      
+      card.append(h2);
+    }
+
+     card.appendChild(name);
+    card.appendChild(p);
+    card.appendChild(p2);
+    card.appendChild(a);
+    card.appendChild(level);
+
+    display.classList.add(type);
+    display.append(card);
+  });
+}
+
+async function getBusinesses(type) {
+  let response = await fetch(URL);
+  if (response.ok) {
+    let data = await response.json();
+    buildBusinessCards(data, type);
+  } else {
+    throw Error(response.statusText);
+  }
+}
+
+function deleteItems() {
+  for (let i = 0; i < 9; i++) {
+    document.getElementById("section").remove();
+  }
+}
+
+getBusinesses("grid");
+
+gridbutton.addEventListener("click", () => {
+  if (display.classList.value == "cards list") {
+    deleteItems();
+    display.classList.remove("list");
+    getBusinesses("grid");
+  }
+});
+
+listbutton.addEventListener("click", () => {
+  if (display.classList.value == "cards grid") {
+    deleteItems();
+    display.classList.remove("grid");
+    getBusinesses("list");
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function toggleMenu(){
     document.getElementById("pirmarynav").classList.toggle("open");
     console.log("Done");
