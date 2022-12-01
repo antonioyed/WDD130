b0c4cc0ed6aa2f38acafad5a6c69e8129a1b2d9c
antonioyed <yed21001@byui.edu>
2022-11-30 19:53:15 -0500

    new

--- wdd230/bountifulfoods/javas/scripts.js
+++ wdd230/bountifulfoods/javas/scripts.js
@@ -1,32 +1,9 @@
 
-
-
 function toggleMenu(){
-    document.getElementById("pirmarynav").classList.toggle("open");
-    console.log("Done");
+    document.getElementById("bountiNav").classList.toggle("open");
+    document.getElementById("bountiBtn").classList.toggle("open");
+   
 }
-const mn = document.getElementById("hamburgerbtn");
-mn.onclick = toggleMenu;
-
-
-// select the elements to manipulate (output to)
-const datefield = document.querySelector(".date");
-const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
-
-// derive the current date using a date object
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(now);
-// long, medium, short options ... try them
-
-datefield.innerHTML = `<em>${fulldate}</em>`;
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
-
-
-
-
+const bou = document.getElementById("bountiBtn");
+bou.onclick = toggleMenu;
 
