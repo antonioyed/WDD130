b4d984ede7311f0b0f348dd4b3c59e3ba9886945
antonioyed <yed21001@byui.edu>
2022-12-02 09:19:09 -0500

    new

--- wdd230/bountifulfoods/js/scripts.js
+++ wdd230/bountifulfoods/js/scripts.js
@@ -7,3 +7,19 @@ function toggleMenu(){
 const bou = document.getElementById("bountiBtn");
 bou.onclick = toggleMenu;
 
+
+const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside"); 
+
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
