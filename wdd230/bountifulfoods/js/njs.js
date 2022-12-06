5f38589605dacb00bd1c0ecfb4eb03c6b8ee4fb1
antonioyed <yed21001@byui.edu>
2022-12-05 19:02:34 -0500

    footer

---
+++ wdd230/bountifulfoods/js/njs.js
@@ -0,0 +1,36 @@
+
+  const date = new Date();
+ const jsYear = document.getElementById("y-js");
+ jsYear.innerHTML = date.getFullYear();
+
+ document.querySelector(
+	"#lastModified"
+).textContent = `Last Update: ${document.lastModified}`;
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
