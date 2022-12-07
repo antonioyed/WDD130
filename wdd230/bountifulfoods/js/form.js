c79ab4becab30f4b605ef539e6b1ca7bb559263f
antonioyed <yed21001@byui.edu>
2022-12-07 17:50:21 -0500

    new

---
+++ wdd230/bountifulfoods/js/form.js
@@ -0,0 +1,29 @@
+
+
+let form = document.getElementById('form')
+ 
+form.addEventListener('submit',(e) => {
+
+    e.preventDefault()
+
+    let firstname = document.getElementById('firstname').value
+
+    let email = document.getElementById('email').value
+
+    let phone = document.getElementById('phone').value
+
+    let instrction = document.getElementById('instrction').value
+
+    if(firstname == "" || email == "" || phone == "" || instrction == ""){
+        alert("Please enter all the fields")
+    }
+    else{
+        document.getElementById('result').innerHTML = `
+        <p> Name: ${firstname}</p>
+        <p> Email: ${email}</p>
+        <p> phone: ${phone}</p>
+        <p> instrctions: ${instrction}</p>
+        `
+    }
+    
+})
