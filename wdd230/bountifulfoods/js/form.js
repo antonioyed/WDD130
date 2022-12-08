e03c5df1b8068a7565bfdc65810e930ba8fa1612
antonioyed <yed21001@byui.edu>
2022-12-08 13:21:45 -0500

    new

--- wdd230/bountifulfoods/js/form.js
+++ wdd230/bountifulfoods/js/form.js
@@ -16,17 +16,27 @@ form.addEventListener('submit',(e) => {
 
     let select = document.getElementById('select').value
 
+    let select2 =document.getElementById('select2').value
+
+    let select3 = document.getElementById('select3').value
+
+    let datea  =document.getElementById('datea').value
+
     if(firstname == "" || email == "" || phone == "" 
-    || instrction == "" || select == ""){
+    || instrction == "" || select == "" || select2 == "" || select3 == ""){
         alert("Please enter all the fields")
     }
     else{
         document.getElementById('result').innerHTML = `
-        <p> Name: ${firstname}</p>
-        <p> Email: ${email}</p>
-        <p> phone: ${phone}</p>
-        <p> instrctions: ${instrction}</p>
-        <p> Fruit: ${select}</p>
+        <h3> <strong> Name </strong>: ${firstname}</h3>
+        <h3><strong> Email </strong>: ${email}</h3>
+        <h3><strong> phone</strong>: ${phone}</he>
+        <h3> <strong>Date</strong>: ${datea}</h3>
+        
+        <h3> <strong>Fruit mix</strong>: ${select}</h3>
+        <h3> <strong>+ </strong>: ${select2}</h3>
+        <h3><strong> +</strong> : ${select3}</h3>
+        <h3> <strong>instrctions</strong>: ${instrction}</h3>
         `
     }
     
