1e193e44a514d1a9207c47d7afbfec4eb5b468dd
antonioyed <yed21001@byui.edu>
2022-12-08 16:00:48 -0500

    new

---
+++ wdd230/bountifulfoods/js/but.js
@@ -0,0 +1,20 @@
+const input =document.getElementById("submitBtn")
+const scoreElement = document.getElementById("score_element")
+
+if(typeof(localStorage) === undefined){
+
+    console.log("Your browser not soport localstorage : C")
+}
+
+if(localStorage.getItem("score")){
+    scoreElement.innerText = localStorage.getItem("score")
+    
+}else{
+    localStorage.setItem("score", "0")
+}
+
+input.submit = ()=>{
+    const currentScore = localStorage.getItem("score")
+    localStorage.setItem("score" , parseInt(currentScore) + 1)
+    scoreElement.innerText = localStorage.getItem("score")
+}
